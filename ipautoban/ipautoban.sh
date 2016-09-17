999ecda1e491d36a57cdc7bfe422e302e84f422a
Pavol Vytykac <pvytykac@pvytykac.net>
2016-09-17 17:41:03 +0200

    * moved logging logic to a standalone reusable script * renamed the bruteforcers script to ipautoban * refactored the ipautoban script to use the newly added logging script

--- ipautoban/ipautoban.sh
+++ ipautoban/ipautoban.sh
@@ -1,59 +1,44 @@
 #!/bin/bash
+DIR=$(dirname "$0")
 
-# redirect output streams
-LOG_FILE=/var/log/bruteforcers/bruteforcers
-exec 1<&-
-exec 2<&-
-exec 1>>"$LOG_FILE.log"
-exec 2>>"$LOG_FILE.err"
-
-function log {
-  MSG="$1" # message to be logged
-  LOG="$2" # stream to append the message to
-
-  # if LOG was not specified default to &1
-  if [ -z $STR ]; then
-    LOG="&1"
-  fi
-
-  # if a message was provided log it
-  if [ ! -z "$MSG" ]; then
-    eval "echo \"$(date -u +'%Y-%m-%d %H:%M:%S').$(date -u +%N | cut -c 1-3)   $MSG\" >$LOG"
-  fi
-}
+# include the log lib
+. $DIR/lib/log.sh
 
 # ban function
 function banIP {
-  IP="$1"
-  ATTEMPTS="$2"
+  local IP="$1"
+  local ATTEMPTS="$2"
 
   # check that IP is defined
   if [ -z $IP ]; then
-    log "$IP: ip not provided" "&2"
-    exit 1
+    error "$IP: ip not provided"
+    return 1
   fi
 
   # check that ATTEMPTS is a number
   if [ ! "$ATTEMPTS" -eq "$ATTEMPTS" ]; then
-    log "$IP: $ATTEMPTS is not a number" "&2"
+    error "$IP: $ATTEMPTS is not a number"
+    return 1
   fi
 
   # firewall rule template
-  RULE="rule family='ipv4' source address='$IP' reject"
-  RULE_FILTER=$(echo "$RULE" | sed "s/'/\"/g")
-  EXISTS=$(firewall-cmd --list-rich-rules | grep "$RULE_FILTER" | wc -l)
+  local RULE="rule family='ipv4' source address='$IP' reject"
+  local RULE_FILTER=$(echo "$RULE" | sed "s/'/\"/g")
+  local EXISTS=$(firewall-cmd --list-rich-rules | grep "$RULE_FILTER" | wc -l)
 
   # if the rule is already specified for the IP log the message and dont add anything
   if [ "$EXISTS" -gt "0" ]; then
-    log "$IP: already banned"
+    trace "$IP: already banned"
+    return 0
   # otherwise add the rule and log the message
   else
-    log "$IP: banning after $ATTEMPTS unsuccessfull login attempts"
+    info "$IP: banning after $ATTEMPTS unsuccessfull login attempts"
     firewall-cmd --permanent --add-rich-rule="$RULE" > /dev/null 2>&2
+    return $?
   fi
 }
 
-log "starting"
+info  "starting"
 
 # parse all the unsuccessfull login attempts from ssh logs
 # if more then $LIMIT login attempts failed from the same IP ban it
@@ -64,10 +49,15 @@ do
 
   if [ "$cnt" -gt "5" ]; then
     banIP $ip $cnt
+    if [ "$?" -ne "0" ]; then
+      error "error encountered while processing the ip address '$ip' with '$cnt' unsuccessfull login attempts"
+    fi
   fi
 done
 
 # reload the firewalld for the changes to take effect
+info "finished the banning process and reloading the firewall"
 firewall-cmd --reload > /dev/null 2>&2
 
-log "finished"
+info "done"
+exit 0
---
+++ log/log.sh
@@ -0,0 +1,81 @@
+# logs a message ($1) at a trace level ($2 - default info)
+# supported log levels:
+#   stdout -> TRACE,DEBUG,INFO,WARN
+#   stdere -> ERROR
+function log {
+  local msg="$1"                              # message to be logged
+  local lvl="$2"                              # log level to log the message at
+  local ts=$(date -u +"%Y-%m-%d %H:%M:%S.%N") # UTC0 timestamp
+  local out="&1"                              # output stream
+  local levels="TRACE:DEBUG:INFO:WARN:ERROR"  # supported levels
+
+  # on empty message
+  if [ -z "$msg" ]; then
+    return 1
+  fi
+
+  # default to INFO if log level was not specified or an unsupported level was specified
+  if [ -z "$lvl" ] || { [ $(echo "$levels" | grep -ic "$lvl:") -eq "0" ] && [ $(echo "$levels" | grep -ic ":$lvl") -eq "0" ]; }; then
+    lvl="INFO"
+  fi
+
+  # if ERROR level was specified change the output stream to stderr
+  if [ "$lvl" == "ERROR" ]; then
+    out="&2"
+  fi
+
+  # allways log the level as uppercase string
+  lvl=$(echo "$lvl" | awk '{ print toupper($0)}')
+
+  # log the message and return the result of eval call
+  eval "printf \"%s\t%s\t%s\n\" \"${ts:0:23}\" \"$lvl\" \"$msg\" >$out"
+  return $?
+}
+
+# convinience functions below
+
+function trace {
+  log "$1" "TRACE"
+}
+
+function debug {
+  log "$1" "DEBUG"
+}
+
+function info {
+  log "$1" "INFO"
+}
+
+function warn {
+ log "$1" "WARN"
+}
+
+function error {
+  log "$1" "ERROR"
+}
+
+# test cases
+
+#trace "this is a trace message"
+#echo "status: $?"
+
+#debug "this is a debug message"
+#echo "status: $?"
+
+#info "this is an info message"
+#echo "status: $?"
+
+#warn "this is a warn message"
+#echo "status: $?"
+
+#error "this is an error message"
+#echo "status: $?"
+
+#log "this is a message with unknown log level, it should default to info" "ABCDEFGH"
+#echo "status: $?"
+
+#log "this is a trace message with lowercase level specified, should be logged at TRACE level" "trace"
+#echo "status: $?"
+
+#info
+#echo "status of a call to info without parameters should be 1: $?"
